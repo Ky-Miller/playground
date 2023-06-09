625b85100715d23688868419c06905c47912a74c
Ky-Miller <kychr.miller@gmail.com>
2023-06-09 13:45:52 -0700

    3rd commit

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/playground/<int:number>/<string:color>')
+@app.route('/playground/')
 def boxes():
         return render_template('index.html', x = 3, boxColor='blue')
-def boxes1(number, color):
+@app.route('/playground/<int:number>/')
+def boxes1(number):
+    return render_template('index.html', x = number, boxColor='blue')
+@app.route('/playground/<int:number>/<string:color>')
+def boxes2(number, color):
     return render_template('index.html', x = number, boxColor=color)
 if __name__=="__main__":
     app.run(debug=True)
